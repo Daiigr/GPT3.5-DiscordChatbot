9e1f6f909a70b4969d741aeb1c3101d51dd7fb86
Daiigr <daiigrbr@gmail.com>
2023-02-01 14:26:52 +0300

    added ConfigParser.py which creates and manages the config.ini file that stores all the api keys

---
+++ ConfigParser.py
@@ -0,0 +1,73 @@
+import configparser
+
+config = configparser.ConfigParser()
+import os
+
+def  create_config():
+    print('config.ini does not exist, creating file')
+    config['API-KEYS'] = {'OPENAI_API_KEY': '<api-key>',
+                        'DISCORD_API_KEY': '<api-key>'}
+
+    config['APICONFIG'] = {'DEFAULT_PERSONALITY_TYPE':'<personality description>' ,
+    'MODEL_TYPE':'<model type>',
+    'MAX_TOKENS': '<max number of tokens>'}
+
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+# if example.ini does not exist in working directory run create_config() 
+if not os.path.exists("config.ini"):
+    create_config()
+else:
+    config.read('config.ini')
+    config.sections()
+   # print(config['API-KEYS']['openai_api_key'])
+# if example.ini exists run 
+
+def get_openai_api_key():
+    if not os.path.exists("config.ini"):
+        create_config()
+        return 
+    else:
+        config.read('config.ini')
+        config.sections()
+        return config['API-KEYS']['openai_api_key']
+
+def get_discord_api_key():
+    if not os.path.exists("config.ini"):
+        create_config()
+        return 
+    else:
+        config.read('config.ini')
+        config.sections()
+        return config['API-KEYS']['openai_api_key']
+
+def get_default_personality_type():
+    if not os.path.exists("config.ini"):
+        create_config()
+        return 
+    else:
+        config.read('config.ini')
+        config.sections()
+        return config['APICONFIG']['DEFAULT_PERSONALITY_TYPE']
+
+def get_model_type():
+    if not os.path.exists("config.ini"):
+        create_config()
+        return 
+    else:
+        config.read('config.ini')
+        config.sections()
+        return config['APICONFIG']['MODEL_TYPE']
+
+def get_max_tokens():
+    if not os.path.exists("config.ini"):
+        create_config()
+        return 
+    else:
+        config.read('config.ini')
+        config.sections()
+        return config['APICONFIG']['MAX_TOKENS']
+    
+
+
